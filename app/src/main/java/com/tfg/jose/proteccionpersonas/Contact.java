7a3eebcaf534bca25e8d0b5a68d28e5978a21592
alu0100697414 <alu0100697414@ull.edu.es>
2016-02-20 19:22:41 +0000

    Creada la clase Contacto.

---
+++ app/src/main/java/com/tfg/jose/proteccionpersonas/Contact.java
@@ -0,0 +1,35 @@
+package com.tfg.jose.proteccionpersonas;
+
+/**
+ * Created by jose on 20/02/16.
+ *
+ * Clase Contacto.
+ */
+
+public class Contact {
+
+    private String name;
+    private int id;
+
+    public Contact(int num, String nm){
+        name = nm;
+        id = num;
+    }
+
+    String getName(){
+        return name;
+    }
+
+    int getId(){
+        return id;
+    }
+
+    void setName(String nm){
+        name = nm;
+    }
+
+    void setId(int num){
+        id = num;
+    }
+
+}
